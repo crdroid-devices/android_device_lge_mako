d9bbfe4b3d647c6d7e6ede72b75ee5ac2d227412
firebird11 <hbgassel@gmail.com>
2016-07-26 12:14:10 +0200

    change back to cm

--- cm.mk
+++ cm.mk
@@ -3,17 +3,17 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
 # Inherit some common crdroid stuff.
-$(call inherit-product, vendor/crdroid/config/common_mini_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/crdroid/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/mako/full_mako.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mako
-PRODUCT_NAME := crdroid_mako
+PRODUCT_NAME := cm_mako
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
